83322a6247b437587d8e5c424864f01b3e04ce80
cnotv <giuseppe.leo@suse.com>
2024-04-29 00:55:19 +0200

    prettify code

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,78 +1,86 @@
-import { defineStore } from "pinia";
-import { socket } from "@/socket";
-import { getStorageItem } from "@/utils/localStorage";
+import { defineStore } from 'pinia'
+import { socket } from '@/socket'
+import { getStorageItem } from '@/utils/localStorage'
 
-const getName = (state: any) => getStorageItem(state, 'name') || `Guest${Math.floor(Math.random() * 1000)}`
+const getName = (state: any) =>
+  getStorageItem(state, 'name') || `Guest${Math.floor(Math.random() * 1000)}`
 
-const defaultUser = () => ({
-  id: Date.now().toString(),
-  name: '',
-  position: {
-    x: 0,
-    y: 0,
-    z: 0,
-  },
-  rotation: {
-    _x: 0,
-    _y: 0,
-    _z: 0,
-  }
-}) as User;
+const defaultUser = () =>
+  ({
+    id: Date.now().toString(),
+    name: '',
+    position: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    rotation: {
+      _x: 0,
+      _y: 0,
+      _z: 0
+    }
+  }) as User
 
-export const useUsersStore = defineStore("user", {
+export const useUsersStore = defineStore('user', {
   state: () => ({
     users: [] as User[],
     user: defaultUser(),
-    messages: [] as Message[],
+    messages: [] as Message[]
   }),
 
   actions: {
     bindEvents() {
       // sync the list of users upon connection
-      socket.on("connect", () => {
-        this.createUser(getName(this.$state));
-      });
+      socket.on('connect', () => {
+        this.createUser(getName(this.$state))
+      })
 
-      socket.on("user:list", ({users, id}: {users: User[], id: string}) => {
+      socket.on('user:list', ({ users, id }: { users: User[]; id: string }) => {
         if (id !== this.user.id) {
-          this.users = users.filter((user) => user.id !== this.user.id);
+          this.users = users.filter((user) => user.id !== this.user.id)
         }
-      });
+      })
 
       // update the store when a message is received
-      socket.on("message:created", (message: Message) => {
-        this.messages.push(message);
-      });
+      socket.on('message:created', (message: Message) => {
+        this.messages.push(message)
+      })
     },
 
     createUser(name: string) {
       const user: User = {
         ...defaultUser(),
-        name,
-      };
-      this.user = user;
-      socket.emit("user:create", user, (newUser: User) => {
-        this.user = newUser;
-      });
+        name
+      }
+      this.user = user
+      socket.emit('user:create', user, (newUser: User) => {
+        this.user = newUser
+      })
     },
 
     changeUserName(name: string) {
-      this.user.name = name;
-      socket.emit("user:change", this.user);
+      this.user.name = name
+      socket.emit('user:change', this.user)
     },
 
-    sendMessage(message: string) {   
-      socket.emit("message:create", { 
+    sendMessage(message: string) {
+      socket.emit('message:create', {
         name: this.user.name,
         id: this.user.id,
-        text: message,
-      });
+        text: message
+      })
     },
 
-    updateUserPosition({position, rotation}: {position: User['position'], rotation: User['rotation']}) {
-      this.user.position = position;
-      this.user.rotation = rotation;
-      socket.emit("user:change", this.user);
-    },
-  },
-});
+    updateUserPosition({
+      position,
+      rotation
+    }: {
+      position: User['position']
+      rotation: User['rotation']
+    }) {
+      this.user.position = position
+      this.user.rotation = rotation
+      socket.emit('user:change', this.user)
+    }
+  }
+})
