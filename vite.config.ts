09f1b97c83a072f9d73730511b779c59f3f99724
cnotv <giuseppe.leo@suse.com>
2024-11-22 17:00:42 +0100

    Remove vue debugging

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools(), wasm()],
+  plugins: [vue(), wasm()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
