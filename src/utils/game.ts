2e145e818e800429477e94cfc7a19549ffda74b8
cnotv <giuseppe.leo@suse.com>
2025-02-01 01:40:51 +0100

    Prevent player to collide against blocks

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,6 +1,39 @@
 import * as THREE from 'three'
 import { updateAnimation } from '@/utils/threeJs'
 
+export const moveForward = (
+  player: UserModel,
+  dynamicBodies: Record<BlockTypes, PhysicObject[]>,
+  distance: number,
+  backwards: boolean = false
+) => {
+  const collision = 2.5
+  const oldPosition = player.model.position.clone() // Clone the current position
+
+  // Calculate the forward vector
+  const forward = new THREE.Vector3()
+  player.model.getWorldDirection(forward)
+  if (backwards) {
+    forward.negate()
+  }
+  forward.multiplyScalar(distance)
+
+  // Create a new position by adding the forward vector to the old position
+  const newPosition = oldPosition.clone().add(forward)
+
+  // Check for collisions with the new position
+  const isColliding = dynamicBodies.blocks.some(({ model }) => {
+    const difference = model.position.distanceTo(newPosition)
+    return difference < collision // Adjust this value based on your collision detection needs
+  })
+
+  if (!isColliding) {
+    // Update the model's position and the rigid body's translation if no collision is detected
+    player.model.position.copy(newPosition)
+    player.rigidBody.setTranslation(newPosition, true)
+  }
+}
+
 /**
  * Move the player and emit new scene information
  */
@@ -16,41 +49,22 @@ export const movePlayer = (
   const { model, mixer, actions } = player
   if (isFocused) {
     const coefficient = 0.01
-    const isColliding = dynamicBodies.blocks.some(({ model }) => {
-      const distance = model.position.distanceTo(player.model.position)
-      return distance < 2.5
-    })
     if (controls.up) {
-      if (!isColliding) {
-        // Calculate the forward vector
-        const forward = new THREE.Vector3()
-        model.getWorldDirection(forward)
-        forward.multiplyScalar(config.speed.move * coefficient)
-
-        // Add the forward vector to the model's position
-        model.position.add(forward)
-      }
+      moveForward(player, dynamicBodies, config.speed.move * coefficient)
     }
     if (controls.down) {
-      if (!isColliding) {
-        // Calculate the forward vector
-        const forward = new THREE.Vector3()
-        model.getWorldDirection(forward)
-        forward.negate()
-        forward.multiplyScalar(config.speed.move * coefficient)
-
-        // Add the forward vector to the model's position
-        model.position.add(forward)
-      }
+      moveForward(player, dynamicBodies, config.speed.move * coefficient, true)
     }
     if (controls.left) {
       model.rotateY(config.speed.rotate * coefficient)
+      player.rigidBody.setRotation(model.quaternion, true)
     }
     if (controls.right) {
       model.rotateY(-config.speed.rotate * coefficient)
+      player.rigidBody.setRotation(model.quaternion, true)
     }
 
-    // TODO: Model is updated only on key press and not on movement, so jump animation is not completed
+    // TODO: Model is updated only on `key press and not on movement, so jump animation is not completed
     if (controls.jump) {
       if (!player.status.jumping) {
         config.velocityY = config.speed.jump * coefficient // Set an upward velocity when the space key is pressed
@@ -61,10 +75,6 @@ export const movePlayer = (
     if (mixer && actions) {
       if (['left', 'right', 'up', 'down', 'jump'].some((action) => controls[action])) {
         updateAnimation(mixer, actions, delta, config.speed.move)
-
-        // Update the rigid body position and rotation
-        player.rigidBody.setTranslation(model.position, true)
-        player.rigidBody.setRotation(model.quaternion, true)
 
         // For some reason the rotation is reported as Euler type but it's not
         updateUserData({ position: model.position, rotation: model.rotation })
